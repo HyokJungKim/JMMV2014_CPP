be5f13aeb635c06d378c245b9850799513c752a1
Junghiskhan <khjkim@ucdavis.edu>
2021-11-19 15:21:51 -0800

    update cpp

--- ScriptJMMV.cpp
+++ ScriptJMMV.cpp
@@ -16,17 +16,6 @@ int main() {
 	t2 = std::chrono::high_resolution_clock::now();
 	duration = std::chrono::duration_cast<std::chrono::milliseconds> (t2 - t1).count();
 	std::cout << "Time Duration of Smolyak_Grid: " << duration << std::endl;
-
-	/*
-	t1 = std::chrono::high_resolution_clock::now();
-	vd in_3 = Smolyak_Polynomial(in_2, in_1);
-	//double *in_3 = Smolyak_Polynomial(in_2, in_1);
-
-	t2 = std::chrono::high_resolution_clock::now();
-	duration = std::chrono::duration_cast<std::chrono::milliseconds> (t2 - t1).count();
-	std::cout << "Duration of Smolyak_Polynomial: " << duration << std::endl;
-	*/
-	//delete[] in_3;
-
+	
 	return 0;
 }
